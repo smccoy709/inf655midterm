bd05702a99da156e6ae065629fc837c79e6c0d2b
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2024-03-12 14:04:54 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ import { ShopContextProvider } from './context/ShopContext';
 function App() {
   return (
     <div className='App'>
+      {/* Created routes to allow entry to the homepage (Shop), cart page (cart), and the checkout page (confirmation page) */}
+      {/* The Shop page is a / directory and is the main page featuring all the products; it will pull from the Shop component */}
+      {/* The Cart page is the shopping cart page (also known as the checkout page) and pulls from the Cart component */}
+      {/* The Checkout page is the confirmation page after a user checks out by filling out their shipping information and reviewing
+      the products and subtotal in the cart and then it pulls that information from the Checkout component; this is completed when the
+      Checkout button is clicked on the Cart page after a user enters their information and wants to checkout and receive their
+      confirmation number */}
       <ShopContextProvider>
         <Router>
           <Navbar />
