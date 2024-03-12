25ecabf6ba4597b998d904aab6c61dc718e1f68f
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2024-03-12 13:47:21 -0400

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ export const Navbar = () => {
     return (
         <div className='navbar'>
             <div className='links'>
+                {/* Link the nav bar on the main page to the defaulted homepage for "Shop" and to the shopping cart page using a linked
+                shopping cart image */}
                 <Link to='/'>Shop</Link>
                 <Link to='/cart'>
                     <ShoppingCart size={32} />
