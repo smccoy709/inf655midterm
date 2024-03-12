bf3900d590472385d98b3b684a399f22c53da83f
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2024-03-12 14:05:58 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+{/* Renders the root and calls the App page to run upon using npm start within the terminal */}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
