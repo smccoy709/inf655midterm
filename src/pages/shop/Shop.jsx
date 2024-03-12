7311f19197c7732f4c8e9c60a7ee62a2f196d544
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2024-03-12 13:56:43 -0400

    Add files via upload

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -8,6 +8,7 @@ export const Shop = () => {
 
     return (
         <div className='shop'>
+        {/* Creating the search textbox and utilizing an onChange event to search for products in the store that have the letters entered */}
         <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
             setSearchTerm(event.target.value);
           }} />
@@ -15,17 +16,22 @@ export const Shop = () => {
                 <h1>Scotty's Retro Video Games</h1>
             </div>
             <div className='products'>
+            {/* Filtering the results (values (val)) to see if there was something searched */}
+            {/* If the search term is equalted to no search terms, display everything in the store */}
+            {/* Else if, the search term had values that are within the product's name, display only those products */}
+            {/* Else, display no results when a result doesn't match a product's name within the store */}
             {
             Products
               .filter((val) => {
-                if(searchTerm == ""){
+                if(searchTerm == "") {
                   return val;
-                }else if(val.productName?.toLowerCase().includes(searchTerm?.toLowerCase())){
+                } else if (val.productName?.toLowerCase().includes(searchTerm?.toLowerCase())) {
                   return val;
                 }
               })
               .map((product) => {
-                return(
+                {/* Display all the products that are in the store by returning the Product component within the shop directory */}
+                return (
                     <Product data={product} />
                 )
               })
