0509a9041feb38e3776c533ae6d12720c0712d9e
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2024-03-22 13:25:33 -0400

    Add files via upload

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -14,7 +14,6 @@ const getDefaultCart = () => {
 export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    {/* Getting the total amount of product (items) are within the cart and their quantities of each product */}
     const getTotalCartAmount = () => {
         let totalAmount = 0;
         for (const item in cartItems) {
@@ -25,11 +24,7 @@ export const ShopContextProvider = (props) => {
         }
         return totalAmount;
     };
-
-    {/* Allowing the addToCart to increment the quantities in the cart */}
-    {/* Allowing the removeFromCart to decrement the quantities in the cart */}
-    {/* Allowing the updateCartItemCount to update the number of quantities in the cart when the + or - buttons are clicked */}
-    {/* Allowing the cart items to be checked out when the Checkout button is clicked */}
+    
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
     };
